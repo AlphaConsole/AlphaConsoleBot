72977a27e913ecaf07d2bbd46012aeee2ca48c95
Hayden <meloche.hayden@gmail.com>
2017-12-04 20:00:19 -0500

    Add files via upload

---
+++ src/AlphaConsole.js
@@ -0,0 +1,9 @@
+//Main file for AlphaConsole Discord Bot
+
+
+//const
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.login(require('./vars.js').TestBotToken);
