e6905948993f60094859cb45112a9950b3a0a133
Hayden Meloche <meloche.hayden@gmail.com>
2017-12-07 23:42:48 -0500

    Added bot watching

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 
 
 module.exports.run = async(client, channels, callback) => {
-
     console.log('AlphaConsole Bot logged in and ready.');
-    
+    client.user.setActivity("@ alphaconsole.net", {type: "WATCHING"});
 }
