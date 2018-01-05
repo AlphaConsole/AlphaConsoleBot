717142e7bd2e223e25213d5480d6dcabc7916df5
Hayden <meloche.hayden@gmail.com>
2018-01-05 15:36:13 -0500

    Delete tokens.js

--- src/tokens.js
+++
@@ -1,6 +0,0 @@
-module.exports.TestBotToken = ''; //input your bot key
-module.exports.BadWordsURL = '';
-module.exports.Password = '';
-module.exports.SetTitleURL = '';
-module.exports.CheckdbURL = '';
-module.exports.CleverBOT = '';
