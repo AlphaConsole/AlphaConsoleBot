a26ce8320e9272a48bb42df9e5a060272c5bc16e
Pieter Jan Pollie <pieterjanpollie@gmail.com>
2018-01-13 21:50:05 +0100

    #98 cases fix works on members not in guild

--- src/cmds/cases.js
+++ src/cmds/cases.js
@@ -10,48 +10,60 @@ module.exports = {
         if(hasRole(message.member, 'Support') || hasRole(message.member, "Moderator") || hasRole(message.member, "Admin") || hasRole(message.member, "Developer")) {
 
             if (message.mentions.users.first() == undefined) {
-                if (message.guild.members.get(args[1]) == undefined) {
+                var TheUser = args[1];
+                
+            } else {
+                var TheUser = message.mentions.users.first().id;
+            }
+
+            await sql.get(`select * from Members where DiscordID = '${TheUser}'`).then(row => {
+                if (!row) {
                     const embed = new Discord.MessageEmbed()
                     .setColor([255,255,0])
                     .setAuthor(`User not found`, serverInfo.logo)
                     return message.channel.send(embed)
                 } else {
-                    var TheUser = message.guild.members.get(args[1]).user;
-                }
-            } else {
-                var TheUser = message.mentions.users.first();
-            }
+                    const embed = new Discord.MessageEmbed()
+                    .setColor([255,255,0])
 
-            const embed = new Discord.MessageEmbed()
-            .setColor([255,255,0])
-            .setAuthor(`All cases of ${TheUser.tag}`, serverInfo.logo)
-
-            sql.all(`select * from logs where Member = '${TheUser.id}' AND Action = 'mute'`).then(mutes => {
-                embed.addField("Mutes", mutes.length, true)
-
-                sql.all(`select * from logs where Member = '${TheUser.id}' AND Action = 'warn'`).then(warns => {
-                    embed.addField("Warnings", warns.length, true)
-                    embed.setThumbnail('http://www.cityrider.com/fixed/43aspect.png')
-
-                    sql.all(`select * from logs where Member = '${TheUser.id}' AND Action = 'kick'`).then(kicks => {
-                        embed.addField("Kicks", kicks.length, true)
-
-                        sql.all(`select * from logs where Member = '${TheUser.id}' AND Action = 'ban'`).then(bans => {
-                            embed.addField("Bans", bans.length, true)
-
-                            sql.all(`select * from logs where Member = '${TheUser.id}' LIMIT 5`).then(cases => {
-
-                                var output = ""
-                                cases.forEach(element => {
-                                    output += "**" + element.ID + '**: ' + capitalizeFirstLetter(element.Action) + " - " + element.Reason + "\n"
-                                });
-                                
-                                embed.addField("Last 5 cases", output)
-                                message.channel.send(embed)
+                    if (client.users.get(TheUser)) {
+                        embed.setAuthor(`All cases of ${client.users.get(TheUser).tag}`, serverInfo.logo)
+                    } else {
+                        embed.setAuthor(`All cases of ${row.Username} (Last name found in db)`, serverInfo.logo)
+                    }
+                    
+        
+                    sql.all(`select * from logs where Member = '${TheUser}' AND Action = 'mute'`).then(mutes => {
+                        embed.addField("Mutes", mutes.length, true)
+        
+                        sql.all(`select * from logs where Member = '${TheUser}' AND Action = 'warn'`).then(warns => {
+                            embed.addField("Warnings", warns.length, true)
+                            embed.setThumbnail('http://www.cityrider.com/fixed/43aspect.png')
+        
+                            sql.all(`select * from logs where Member = '${TheUser}' AND Action = 'kick'`).then(kicks => {
+                                embed.addField("Kicks", kicks.length, true)
+        
+                                sql.all(`select * from logs where Member = '${TheUser}' AND Action = 'ban'`).then(bans => {
+                                    embed.addField("Bans", bans.length, true)
+        
+                                    sql.all(`select * from logs where Member = '${TheUser}' LIMIT 5`).then(cases => {
+        
+                                        var output = ""
+                                        cases.forEach(element => {
+                                            output += "**" + element.ID + '**: ' + capitalizeFirstLetter(element.Action) + " - " + element.Reason + "\n"
+                                        });
+                                        
+                                        if (output.length < 2) {
+                                            output = "/"
+                                        } 
+                                        embed.addField("Last 5 cases", output)
+                                        message.channel.send(embed)
+                                    })
+                                })
                             })
                         })
                     })
-                })
+                }
             })
         }  
     }
