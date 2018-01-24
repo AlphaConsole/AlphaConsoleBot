a283f5afcbcf9adaa80b1d0cc3e48d54573abd3b
ThePjpollie <Pieterjan.pollie@gmail.com>
2018-01-24 03:29:49 +0100

    Added serverinfo command. Closes #106

--- src/AlphaConsole.js
+++ src/AlphaConsole.js
@@ -37,12 +37,12 @@ client.on('ready', () => {
 //New member joins
 client.on('guildMemberAdd', (member) => {
     require('./events/newMember.js').run(client, serverInfo, member, sql);
-}); 
+});
 
 //User Left / kicked
 client.on('guildMemberRemove', (member) => {
     require('./events/guildMemberRemove.js').run(client, serverInfo, member, sql);
-}); 
+});
 
 //Voice users update
 client.on('voiceStateUpdate', (oldMember, newMember) => {
@@ -100,7 +100,11 @@ client.on('message', async message =>
             if (args[0].toLowerCase() == "!role") {
                 require('./cmds/role.js').run(client, serverInfo, sql, message, args)
             }
-            
+
+            if (args[0].toLowerCase() == "!serverinfo") {
+                require('./cmds/serverInfo.js').run(client, serverInfo, sql, message, args)
+            }
+
 
             //Help command
             if (args[0].toLowerCase() == "!help" || args[0].toLowerCase() == "!h") {
@@ -227,13 +231,13 @@ client.on('message', async message =>
             else if (args[0].toLowerCase() == "!listroles") {
                 require('./cmds/listroles.js').run(client, serverInfo, sql, message, args)
             }
-            
+
             /// ADMIN COMMANDS
             //Admin Bot Status
             else if (args[0].toLowerCase() == "!status") {
                 require('./cmds/status.js').run(client, serverInfo, sql, message, args)
             }
-            
+
             //Disables all channels which rely on the bot heavily. (#set-title, special title, etc)
             else if (args[0].toLowerCase() == "!lockdown") {
                 require('./cmds/lockdown.js').run(client, serverInfo, message, args)
@@ -244,11 +248,11 @@ client.on('message', async message =>
 
             else if (args[0].toLowerCase() == "!blacklist") {
                 require('./cmds/blacklist.js').run(client, serverInfo, message, args, sql, blackListedWords)
-            } 
+            }
 
             else if (args[0].toLowerCase() == "!update") {
                 require('./cmds/update.js').run(client, serverInfo, message, args)
-            } 
+            }
 
             //For commands with 2 args.
             else if (args.length == 2) {
@@ -274,7 +278,7 @@ client.on('message', async message =>
 
     }
 
-    
+
 })
 
 var schedule = require('node-schedule');
