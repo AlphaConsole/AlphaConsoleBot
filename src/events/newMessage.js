b9ce16dc2071010ffee67da017e73af7f61ea165
HaydenMeloche <meloche.hayden@gmail.com>
2018-03-22 09:50:56 -0400

    I'm actually dumb

--- src/events/newMessage.js
+++ src/events/newMessage.js
@@ -71,13 +71,12 @@ module.exports = {
                 } else {
                   if (row.ccCooldown < new Date().getTime()) {
 
-                    var getID;
+                    var theUser;
                     if (message.mentions.users.first() != undefined) {
-                       getID = message.mentions.users.first();
+                       theUser = message.mentions.users.first();
                     }
 
-                    if (getID != undefined) {
-                      theUser = message.guild.members.get(getID);
+                    if (theUser != undefined) {
                       message.channel.send(theUser + " " + command.Response);
                     } else {
                       message.channel.send(command.Response);
