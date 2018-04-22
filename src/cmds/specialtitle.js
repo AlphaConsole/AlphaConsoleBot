13cacaca952b510416a2af1d44f40ade37329475
Hayden <meloche.hayden@gmail.com>
2018-04-22 07:14:09 -0400

    Added a special title doc

---
+++ src/cmds/specialtitle.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: "Special title",
+    perms: "Moderator+",
+    commands: [
+        "!set special <id>"
+      ],
+      description: [
+        "Sets your title to the ID you choose."
+      ]
+    //This is only here to provide documentation in the help command.
+};
