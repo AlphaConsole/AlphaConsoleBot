620aa8554e6273010c82910be0d6020264caf073
Hayden <meloche.hayden@gmail.com>
2018-05-01 09:29:49 -0400

    Forgot comment

--- src/AlphaConsole.js
+++ src/AlphaConsole.js
@@ -44,7 +44,7 @@ client.on("ready", () => {
     SwearWordsSet,
     blackListedWords
   );
-  require("./events/StatusUpdate.js").run(client, serverInfo, sql, require("./tokens.js").TwitchClientID);
+  //require("./events/StatusUpdate.js").run(client, serverInfo, sql, require("./tokens.js").TwitchClientID);
   //require('./events/updatePartners.js').run(client, serverInfo, sql);
 });
 
