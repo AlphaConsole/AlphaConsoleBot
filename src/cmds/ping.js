f8d831140a53e12d2cf821da55a44a0ec3d901aa
ThePjpollie <Pieterjan.pollie@gmail.com>
2018-05-23 15:18:16 +0200

    5 Seconds cd on ping

--- src/cmds/ping.js
+++ src/cmds/ping.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+var cd = 0;
 
 module.exports = {
     title: "ping",
@@ -7,8 +8,12 @@ module.exports = {
     description: ["Check latency of the bot"],
 
     run: async (client, serverInfo, message) => {
+
+        if (cd > new Date().getTime()) return;
+
         message.channel.send("Pinging...").then(m => {
             m.edit(`🏓 Pong! Latency is \`${ m.createdTimestamp - message.createdTimestamp }ms\`.`);
+            cd = new Date().getTime() + 5000;
         })
     }
 };
