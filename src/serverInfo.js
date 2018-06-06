6519d95cb8083d35167ef194cef6735ce3d21c15
ThePjpollie <Pieterjan.pollie@gmail.com>
2018-06-06 05:42:00 +0200

    Changed to beta serverinfo

--- src/serverInfo.js
+++ src/serverInfo.js
@@ -1,12 +1,13 @@
 //Server Information
-module.exports.serverInfo = {
+module.exports = {
   guildName: "AC Beta",
   guildId: "348214140889989140",
   
   roles: {
-    developer: "307608488375222272",
-    admin: "401359549308600321",
-    support: "453598567681228810",
-    staff: "453598542234386432"
+    developer: "381845745147314186",
+    admin: "381845798331088896",
+    moderator: "381845817327353856",
+    support: "381845840412540930",
+    staff: "388523448839831566"
   }
 };
