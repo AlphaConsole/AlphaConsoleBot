1c80412cc1e64735090450abc99d3cb926a8f585
ThePjpollie <Pieterjan.pollie@gmail.com>
2018-06-17 00:42:20 +0200

    Official Discord serverinfo set

--- src/serverInfo-OfficialDiscord.js
+++ src/serverInfo-OfficialDiscord.js
@@ -2,27 +2,48 @@
 module.exports = {
   guildName: "AlphaConsole",
   guildId: "278333760666075136",
-  aclogChannel: "352861631128272897",
-  serverlogChannel: "364569370065895424",
-  modlogChannel: "371462852063789068",
-  DynamicCat: "355924371367395329",
-  BotSpam: "420659899744124928",
-  EventsRole: "389234045709582337",
-  suggestionsChannel: "352855430315376640",
-  showcaseChannel: "358833347486810112",
-  titleReporting: "446186367303680000",
-  ingameReports: "446186367303680000",
-  betaSteamIDS: "423943897329303552",
-  setTitleChannel: "328236864534216704",
-  setSpecialTitleChannel: "363137607540015106",
-  staffChannel: "325079897209044993",
-  basementChannel: "385890535753383956",
-  devChannel: "377980232914698251",
-  editPartnerChannel: "445304697755009024", // Channel for !partner command
-  partnerChannel: "445325924771037185", // Channel bot uses to list the partners, and watch their respective reactions
-  partnerEmoji: ":ACQuestion:445304272603709461", // Emoji used for partner reactions
-  giveawaychannel: "375694017649704960",
-  giveawayrole: "389234045709582337",
-  legacyrole: "278334510221885451",
-  mutedReason: "448536437453881355"
+  
+  roles: {
+    developer : "278334037133754369",
+    admin     : "333374037998960640",
+    moderator : "341650875649097728",
+    support   : "304709520985292802",
+    staff     : "377965712532045825",
+    ch        : "335563840551125002",
+    streamTeam: "386697815251877898",
+
+    legacy    : "278334510221885451",
+    sub       : "359910776397496330",
+    orgPartner: "354507027017629697",
+    partnerP  : "360929233633280000",
+    donator   : "369347889383735297",
+
+    events    : "389234045709582337",
+    muted     : "318485165687177227"
+  },
+
+  channels: {
+    basement              : "xxxx",
+    staff                 : "325079897209044993",
+    slaughter             : "378420714941972480",
+    serverlog             : "364569370065895424",
+    aclog                 : "352861631128272897",
+    modlog                : "371462852063789068",
+    ingameReports         : "446186367303680000",
+    editPartners          : "445304697755009024",
+    botConfig             : "377992732418768907",
+
+    setTitle              : "328236864534216704",
+    setSpecialTitle       : "363137607540015106",
+    betaSteamIDS          : "423943897329303552",
+    
+    botSpam               : "420659899744124928",
+    showcase              : "358833347486810112",
+    suggestion            : "352855430315376640",
+    partners              : "445325924771037185",
+    muted                 : "448536437453881355",
+  },
+
+  partnerEmoji: ":ACQuestion:445304272603709461",
+  DynamicCat: "355924371367395329"
 };
