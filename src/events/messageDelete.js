7d0697c18f94f3bb5906edb46d7c7c3bfc1811dd
ThePjpollie <pieterjan.pollie@gmail.com>
2018-07-07 16:56:48 +0200

    added id to message removal message #235

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -14,6 +14,6 @@ module.exports.run = (client, serverInfo, config, message) => {
     let channel = message.channel.type === "text" ? `<#${message.channel.id}>` : '**DM**';
     client.guilds.get(serverInfo.guildId).channels.get(serverInfo.channels.serverlog).send(
         `:pencil: \`[${new Date().toTimeString().split(" ")[0]}]\` ` + 
-        `**Channel: ${channel} ${message.author.tag}**'s message was deleted. Content: ${ResponseText}`
+        `**Channel: ${channel} ${message.author.tag}**'s (${message.author.id}) message was deleted. Content: ${ResponseText}`
     );
 }
