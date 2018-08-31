5f649bb8b08e6b7f60c5c1b8ea85ed0b49f1f0cd
ThePjpollie <pieterjan.pollie@gmail.com>
2018-08-31 14:16:24 +0200

    Checkdb update

--- src/cmds/checkdb.js
+++ src/cmds/checkdb.js
@@ -24,39 +24,17 @@ module.exports = {
 
         try {
             var id = message.mentions.users.first() ? message.mentions.users.first().id : args[1];
-            var url = config.keys.CheckdbURL + "?DiscordID=" + id;
-            request({ method: "GET", url: url }, function (err, response, body) {
-                if (err) {
-                    let errorCode = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
-                    console.error(`Error code ${errorCode} by ${message.author.tag}`, err);
-                    return sendEmbed(message.author, "🚫 An error occurred. Please contact Pollie#0001. Error code: `" + errorCode + "`");
-                }
-
-                let result = "";
-
-                if (body) {
-                    if (body.toLowerCase().includes("not signed up for db")) {
-                        sendEmbed(message.channel, "An error occured", `It appears you have not signed up for our title service. Please click this link and makes sure you are logging in with the correct account.\n\nhttp://www.alphaconsole.net/auth/index.php`)
-                        
-                    } else if (body.toLowerCase().includes("no title set")) {
-                        sendEmbed(message.channel, "An error occured", `No title set. Go to #set-title and set a title!`);
-                            
-                    } else {
-                        var info = body.split(" ");
-                        var colour = info[info.length - 3];
-                        var steamID = info[info.length - 2];
-                        for (let index = 0; index < info.length - 3; index++) result += info[index] + " ";
-
-                        if (result.trim() == "X" && returnColour(colour) == "Cycling Colours") {
-                            sendEmbed(message.channel, "Database Check", `User: <@${id}>\nSteam: https://steamcommunity.com/profiles/${steamID}\nInformation: User has disabled their title.`);
-                        } else {
-                            sendEmbed(message.channel, "Database Check", `User: <@${id}>\nSteam: https://steamcommunity.com/profiles/${steamID}\nTitle: ${result.trim()}\nColour: ${returnColour(colour)}`);                        
-                        }
-                    }
-                } else {
-                    sendEmbed(message.channel, "An error occured", "There was an error. Please try again. If this problem continues please contact an admin.")
-                }
-            });
+            sql.query("Select * from Players where DiscordID = ? OR SteamID = ?", [ id, id ], (err, rows) => {
+                if (err) return console.error(err);
+                const user = rows[0];
+                
+                if (!user)
+                    return sendEmbed(message.channel, "An error occured", "This user was not found in the database. Here is the info for him to know:\n\n```It appears you have not signed up for our title service. Please click this link and makes sure you are logging in with the correct account.\n\nhttp://www.alphaconsole.net/auth/index.php```")
+        
+                let lastSeen = user.LastSeen ? new Date(user.LastSeen*1000).toUTCString() : "never";
+                let betaUntil = user.BetaUntil ? new Date(user.BetaUntil*1000).toUTCString() : undefined;
+                sendEmbed(message.channel, "Database Check", `Discord: <@${user.DiscordID}>\nSteam: [${user.SteamID}](https://steamcommunity.com/profiles/${user.SteamID})\nLast seen: ${lastSeen}\n\nTitle: ${user.Title}\nColor: #${user.Color}\nGlow: #${user.GlowColor}${ user.Title === "X" || user.Color === "X" ? "\n__Information:__ This user has his title disabled.\n" : "\n" }${ betaUntil ? "\nBeta until: " + betaUntil : "" }\n**[All details of this user](http://staff.alphaconsole.net/details/${user.DiscordID})**`);            
+            })
         } catch (error) {
             console.log(error)
         }
