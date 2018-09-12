f20b88933f72c0a4e7cd114986ecd09702a808b5
ThePjpollie <pieterjan.pollie@gmail.com>
2018-09-12 15:58:06 +0200

    Set title bugfix

--- src/cmds/settitle.js
+++ src/cmds/settitle.js
@@ -252,9 +252,9 @@ module.exports = {
 
             sql.query("Select * from Config where Config = 'blacklistedWords'", [], (err, rows) => {
               if (err) return console.log(err);
-              const thisBlacklist = rows.filter(r => !exemptWords.includes(r.Value1.toLowerCase()))
+              const thisBlacklist = rows.map(r => r.Value1).filter(r => !exemptWords.includes(r.toLowerCase()))
         
-              let blacklistedTitles = titles.filter(t => thisBlacklist.find(b => b && typeof b === "string" && t.toLowerCase().includes(b.toLowerCase())));
+              let blacklistedTitles = titles.filter(t => thisBlacklist.find(b => t.toLowerCase().includes(b.toLowerCase())));
               if (blacklistedTitles.length > 0) {
                 saveTitleToLog(id, title, true, sql);
                 return reject("Your custom title was not set because it contained a blacklisted phrase. \n" +
