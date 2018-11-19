72cc4ae7a0da270b10722d130d880eec9eb43f53
Barbosicks <almeidamv@gmail.com>
2018-11-19 00:22:04 +0000

    I'm really good at js

--- src/cmds/disable.js
+++ src/cmds/disable.js
@@ -14,34 +14,42 @@ module.exports = {
     ],
 
     run: ({ client, serverInfo, message, args, sql, config, sendEmbed }) => {
-
-        if (message.channel.id !== serverInfo.channels.setTitle) return;
-        const keys = config.keys;
-        const user = message.author;
-
-        url = keys.SetTitleURL +
-            "?DiscordID=" + user.id +
-            "&key=" + keys.Password +
-            "&title=X" +
-            "&color=X";
-        request({ method: "GET", url: url }, function(err, response, body) {
-            if (err) {
-                let errorCode = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
-                console.error(`Error code ${errorCode} by ${message.author.tag}`, err);
-                return sendEmbed(message.author, "🚫 An error occurred. Please contact Pollie#0001. Error code: `" + errorCode + "`");
-            }
-
-            if (body) {
-                if (body.toLowerCase().includes("done")) {
-                    sendEmbed(user, "Your title has been disabled.");
-                } else if (body.toLowerCase().includes("the user does not exist")) {
-                    sendEmbed(user, "Hi, in order to use our custom title service you must authorize your discord account. \n" +
-                    "Please click this link: http://alphaconsole.net/auth/index.php and login with your discord account.");
-                }
-            } else 
-                sendEmbed(user, "There was an error. Please try again. If this problem continues please contact an admin.");
-        })
-
+		switch (args[1].toLowerCase()) {
+		  case "title":
+		  default:
+			if (message.channel.id !== serverInfo.channels.setTitle) return;
+			const keys = config.keys;
+			const user = message.author;
+	
+			url = keys.SetTitleURL +
+				"?DiscordID=" + user.id +
+				"&key=" + keys.Password +
+				"&title=X" +
+				"&color=X";
+			request({ method: "GET", url: url }, function(err, response, body) {
+				if (err) {
+					let errorCode = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
+					console.error(`Error code ${errorCode} by ${message.author.tag}`, err);
+					return sendEmbed(message.author, "🚫 An error occurred. Please contact Pollie#0001. Error code: `" + errorCode + "`");
+				}
+	
+				if (body) {
+					if (body.toLowerCase().includes("done")) {
+						sendEmbed(user, "Your title has been disabled.");
+					} else if (body.toLowerCase().includes("the user does not exist")) {
+						sendEmbed(user, "Hi, in order to use our custom title service you must authorize your discord account. \n" +
+						"Please click this link: http://alphaconsole.net/auth/index.php and login with your discord account.");
+					}
+				} else 
+					sendEmbed(user, "There was an error. Please try again. If this problem continues please contact an admin.");
+			})
+			break;
+		  case "banner":
+			sql.query("UPDATE Players SET Banner = '' WHERE DiscordID = ?", [ message.author.id ]);
+			sendEmbed(user, "Your banner has been disabled.");
+			break;
+		}
+      
 
     }
 };
