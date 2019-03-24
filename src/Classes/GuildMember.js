a0db4283676bf8c50070ae250c87567872b2ff1b
ThePjpollie <pieterjan.pollie@gmail.com>
2019-03-24 16:04:54 +0100

    Added fetchInfo on GuildMember

--- src/Classes/GuildMember.js
+++ src/Classes/GuildMember.js
@@ -17,5 +17,20 @@ module.exports = GuildMember => {
           this.is[role] = this.roles.has(config.roles[role]);
       }
     }
+
+    async fetchInfo() {
+      const client = this.client;
+
+      return (this.info = await client.models.Members.findOne({
+        where: {
+          DiscordID: this.id
+        },
+        include: [
+          { model: client.models.Logs, as: "cases" },
+          { model: client.models.Players, as: "steams" },
+          { model: client.models.Titles, as: "titleInfo" }
+        ]
+      }));
+    }
   };
 };
