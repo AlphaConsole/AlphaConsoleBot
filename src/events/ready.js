ce46826080da441d4dc6deba5150c4d06cc7c04b
ThePjpollie <pieterjan.pollie@gmail.com>
2019-03-25 21:58:41 +0100

    Loaded log channels

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,4 +3,6 @@ module.exports = client => {
     client.logChannel = client.channels.get(client.config.logChannel);
 
   client.logSuccess("Bot online!");
+  client.serverLog = client.channels.get(client.config.channels.serverLog);
+  client.acLog = client.channels.get(client.config.channels.acLog);
 };
