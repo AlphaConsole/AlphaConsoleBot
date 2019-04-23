6175a5d957900f02c5bdbc3adf6f96a6fac2029d
Arxarius <arxarius@gmail.com>
2019-04-23 20:47:19 +0100

    Update update.js

--- src/cmds/update.js
+++ src/cmds/update.js
@@ -8,18 +8,24 @@ const Discord = require('discord.js');
 var shell = require("shelljs");
 
 module.exports = {
-     title: "Update",
-     details: [
-        {
-            perms      : "Admin",
-            command    : "!Update",
-            description: "Updates the bot"
-        }
-    ],
+    title: "Update",
+    details: [{
+        perms: "Admin",
+        command: "!Update",
+        description: "Updates the bot"
+    }],
 
-    run: ({ client, serverInfo, message, args, sql, config, sendEmbed }) => {
+    run: ({
+        client,
+        serverInfo,
+        message,
+        args,
+        sql,
+        config,
+        sendEmbed
+    }) => {
 
-        if (message.author.id === "136607366408962048" || message.author.id === "149223090134450177") {
+        if (message.author.id === "136607366408962048" || message.author.id === "131926391909253120") {
             // <---   If you would like to change role perms. Change [BontControl] to your role name
             // client.guilds.get(serverInfo.guildId).channels.get(serverInfo.setTitleChannel).overwritePermissions(message.guild.id, {
             //     SEND_MESSAGES: false
@@ -34,7 +40,7 @@ module.exports = {
             //     SEND_MESSAGES: false
             // });
             //message.channel.send('Channels locked. Executing shell commands...')
-    
+
             //Shell commands
             message.reply("Begining update").then(() => {
                 shell.exec("git checkout .");
